b982322621966ef9ca0775bd95bc5d36c641af9f
ghostt3x <hamzaanest@gmail.com>
2022-05-12 08:56:25 +0100

    4.5v

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,12 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  public loginUser(user :User):Observable<any> {
+
+
+  }
 
  
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,11 +1,14 @@
-export class User{
-    constructor(
-        username:string,
-        email:string,
-        password:string
-    ){}
+export class User {
+    id:number;
+    username:string;
+    email:string;
+    password:string
 
+    constructor(id:number, username:string ,email:string, password:string){
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
 
-
-
+    }
 }
